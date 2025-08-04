8d139b55a9a0727bb04a0eae22ab7f081e0cb3c4
Franck Theeten <franck.theeten@africamuseum.be>
2025-08-04 14:25:34 +0200

    Add files via upload

--- change_PDF_background/correct_pdf_hsv.py
+++ change_PDF_background/correct_pdf_hsv.py
@@ -18,19 +18,20 @@ layout=None
 
 filename=None
 CURRENT_IMAGE=None
+CURRENT_IMAGE_BEFORE_BORDER=None
 CURRENT_HSV_IMAGE=None
 CURRENT_LOWER_HSV=[]
 CURRENT_UPPER_HSV=[]
 display=None
 
 lab_image=None
-maxwidth, maxheight = 800, 1000
+maxwidth, maxheight = 700, 1000
 currentwidth_truncate, currentheight_truncate=0,0
 input_current_width=None
 input_current_height=None
 input_zoom=None
 zoom_slider=None
-ratio=0.0
+ratio=None
 
 slider_h_min=None
 slider_s_min=None
@@ -53,8 +54,18 @@ h_max=None
 s_max=None
 v_max=None
 
-lower_white=[0,0,216]
-upper_white=[141,111,255]
+BORDER_UP=0
+BORDER_DOWN=0
+BORDER_LEFT=0
+BORDER_RIGHT=0
+
+input_border_top=None
+input_border_bottom=None
+input_border_left=None
+input_border_right=None
+
+lower_white=(0,0,230)
+upper_white=(255,255,255)
 
 CURRENT_PAGE=0
 IMG_LIST=[]
@@ -95,8 +106,11 @@ def display_simple(ROI):
     ref_width= ROI.shape[1]
     print(ref_height)
     print(ref_width)
-    if ref_width>maxwidth:
-        ratio=maxwidth/ref_width
+    if ratio is None:
+        if ref_height>maxwidth:
+            ratio=maxwidth/ref_width
+            if ref_height*ratio>maxheight:
+                ratio=maxheight/ref_height
         #print(ratio)
         
         #display_width=math.floor(ref_width/ratio)
@@ -105,18 +119,20 @@ def display_simple(ROI):
         #display = resize(ROI, (display_width, maxheight))
         #ROI=cv2.cvtColor(ROI, cv2.COLOR_BGR2HSV)
         #ROI=cv2.cvtColor(ROI, cv2.COLOR_HSV2BGR)
-        display = cv2.resize(ROI, (0,0), fx=ratio, fy=ratio)        
-        #PanZoomWindow(display,"test")
-        cv2.imshow("",display)
-        currentheight_truncate=display.shape[0]
-        currentwidth_truncate=display.shape[1]
-        
-    else:
-        #PanZoomWindow(ROI,"test")
-        cv2.imshow("",ROI)
-        ratio=1
-        currentheight_truncate=ROI.shape[0]
-        currentwidth_truncate=ROI.shape[1]
+    display = cv2.resize(ROI, (0,0), fx=ratio, fy=ratio)        
+    #PanZoomWindow(display,"test")
+    cv2.imshow("",display)
+    currentheight_truncate=display.shape[0]
+    currentwidth_truncate=display.shape[1]
+    
+    
+    
+    #else:
+    #    #PanZoomWindow(ROI,"test")
+    #    cv2.imshow("",ROI)
+    #    ratio=1
+    #    currentheight_truncate=ROI.shape[0]
+    #    currentwidth_truncate=ROI.shape[1]
     input_zoom.setText(str(round(ratio,5)))
     zoom_slider.setValue(int(ratio*100))
 
@@ -146,7 +162,9 @@ def fnext():
     global CURRENT_IMAGE
     global lab_page
     global display
+    global ratio
     if CURRENT_PAGE<len(IMG_LIST):
+        ratio=None
         CURRENT_PAGE=CURRENT_PAGE+1
         print(CURRENT_PAGE)
         CURRENT_IMAGE =IMG_LIST[CURRENT_PAGE]      
@@ -162,7 +180,9 @@ def fprevious():
     global CURRENT_IMAGE
     global lab_page
     global display
+    global ratio
     if CURRENT_PAGE>0:
+        ratio=None
         CURRENT_PAGE=CURRENT_PAGE-1
         print(CURRENT_PAGE)
         CURRENT_IMAGE =IMG_LIST[CURRENT_PAGE]      
@@ -185,7 +205,7 @@ def fsave_hsv():
         head, tail = os.path.split(filename)    
         new_name=tail.split(".")
         new_name=new_name[0]
-        new_name=new_name+"_"+str(CURRENT_PAGE)+".tif"    
+        new_name=new_name+"_"+str(CURRENT_PAGE)+"_min_"+str(CURRENT_LOWER_HSV[0])+"_"+str(CURRENT_LOWER_HSV[1])"_"+str(CURRENT_LOWER_HSV[2])+"_max_"+str(CURRENT_UPPER_HSV[0])+"_"+str(CURRENT_UPPER_HSV[1])"_"+str(CURRENT_UPPER_HSV[2])+".tif"    
         new_name= os.path.join(OUTPUT_FOLDER[0], new_name)
         cv2.imwrite(new_name, CURRENT_HSV_IMAGE)
         print("saved "+new_name)
@@ -197,7 +217,10 @@ def load_images():
     global CURRENT_IMAGE
     global display
     try:
-        CURRENT_IMAGE =IMG_LIST[CURRENT_PAGE]        
+        CURRENT_IMAGE =IMG_LIST[CURRENT_PAGE]  
+        height, width, channels = CURRENT_IMAGE.shape
+        print("height "+str(height))
+        print("width "+str(width))        
         display=CURRENT_IMAGE.copy()
         display_simple(display)
         lab_page.setText("Page"+str(CURRENT_PAGE+1)+"/"+str(len(IMG_LIST)))
@@ -230,6 +253,7 @@ def slider_do_zoom(val):
     
 def hsv_full_image_job(p_lower_white, p_upper_white ):    
     global CURRENT_IMAGE
+    global CURRENT_IMAGE_BEFORE_BORDER
     global CURRENT_HSV_IMAGE
     black_pixels_ori = np.where(
             (CURRENT_IMAGE[:, :, 0] == 0) & 
@@ -255,6 +279,7 @@ def hsv_full_image_job(p_lower_white, p_upper_white ):
     result[black_pixels] = [255, 255, 255]
     result[black_pixels_ori] = [0, 0, 0]
     CURRENT_HSV_IMAGE=result
+    CURRENT_IMAGE_BEFORE_BORDER=CURRENT_HSV_IMAGE.copy()
     
 def hsv_job(p_lower_white, p_upper_white ):
     global display
@@ -264,7 +289,6 @@ def hsv_job(p_lower_white, p_upper_white ):
             (display[:, :, 2] == 0)
     )
     hsv = cv2.cvtColor(display, cv2.COLOR_BGR2HSV)
-
     mask = cv2.inRange(hsv, p_lower_white, p_upper_white)
         #mask = 255-mask
     mask=cv2.bitwise_not(mask)
@@ -375,12 +399,79 @@ def choose_output():
     file= QFileDialog.getExistingDirectory(window, "Choose folder to add")
     OUTPUT_FOLDER=[file]
     lab_folder.setText("Output folder "+str(OUTPUT_FOLDER[0]))
+  
+def draw_border():
+    global input_border_top
+    global input_border_bottom
+    global input_border_left
+    global input_border_right
+    global CURRENT_HSV_IMAGE  
+    global CURRENT_IMAGE_BEFORE_BORDER
+    global display
     
+    if CURRENT_IMAGE_BEFORE_BORDER is not None:
+        CURRENT_HSV_IMAGE=CURRENT_IMAGE_BEFORE_BORDER
+    
+        
+    height, width, channels = CURRENT_HSV_IMAGE.shape
+    
+    color = (255, 255, 255)
+    input_top=int(input_border_top.text())
+    input_left=int(input_border_left.text())
+    input_bottom=int(input_border_bottom.text())
+    input_right=int(input_border_right.text())
+    if input_top>0:
+        start_point_top=(0,0)
+        end_point_top=(width, input_top)
+        #print("top")
+        #print(start_point_top)
+        #print(start_point_top)
+        CURRENT_HSV_IMAGE = cv2.rectangle(CURRENT_HSV_IMAGE, start_point_top, end_point_top, color, -1) 
+    if input_left>0:
+        start_point_left=(0,0)        
+        end_point_left=(input_left, height)
+        #print("left")
+        #print(start_point_left)
+        #print(end_point_left)
+        CURRENT_HSV_IMAGE = cv2.rectangle(CURRENT_HSV_IMAGE, start_point_left, end_point_left, color, -1)
+    if input_bottom>0:        
+        start_point_bottom=(0,height-input_bottom)
+        end_point_bottom=(width, height)
+        #print("bottom")
+        #print(start_point_bottom)
+        #print(end_point_bottom)        
+        CURRENT_HSV_IMAGE = cv2.rectangle(CURRENT_HSV_IMAGE, start_point_bottom, end_point_bottom,  color, -1) 
+    if input_right>0:        
+        start_point_right=(width-input_right,0)
+        end_point_right=(width, height)
+        #print("right")
+        #print(start_point_right)
+        #print(end_point_right)
+        CURRENT_HSV_IMAGE = cv2.rectangle(CURRENT_HSV_IMAGE, start_point_right, end_point_right, color, -1)        
+    display=CURRENT_HSV_IMAGE.copy()
+    display_simple(display)
     
 def apply_current_hsv():
     global CURRENT_LOWER_HSV
-    global CURRENT_UPPER_HSV
+    global CURRENT_UPPER_HSV  
+    global CURRENT_IMAGE
+    global lower_white
+    global upper_white
+    height, width, channels = CURRENT_IMAGE.shape
+    print("height "+str(height))
+    print("width "+str(width))
+    if CURRENT_LOWER_HSV is None:
+        CURRENT_LOWER_HSV=[]
+    if CURRENT_UPPER_HSV is None:
+        CURRENT_UPPER_HSV=[]
+    if len(CURRENT_LOWER_HSV)==0:
+        CURRENT_LOWER_HSV=lower_white
+    if len(CURRENT_UPPER_HSV)==0:
+        CURRENT_UPPER_HSV=upper_white
+    
     hsv_job(CURRENT_LOWER_HSV, CURRENT_UPPER_HSV)
+    hsv_full_image_job(CURRENT_LOWER_HSV, CURRENT_UPPER_HSV )
+    draw_border()
     
 def start():
     global app
@@ -408,6 +499,20 @@ def start():
     global slider_v_max
     
     global lab_folder
+    
+    global BORDER_UP
+    global BORDER_DOWN
+    global BORDER_LEFT
+    global BORDER_RIGHT
+    
+    global input_border_top
+    global input_border_bottom
+    global input_border_left
+    global input_border_right
+    
+    global lower_white
+    global upper_white
+    
     try:
         app = QApplication([])
         window = QWidget()
@@ -467,27 +572,62 @@ def start():
         
         input_h_min=QLineEdit()
         slider_h_min=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "H Min",slider_h_min, input_h_min, 0, 179)
+        slider_hsv(layout, "H Min",slider_h_min, input_h_min, lower_white[0], 179)
         
         input_s_min=QLineEdit()
         slider_s_min=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "S Min",slider_s_min, input_s_min, 0, 255)
+        slider_hsv(layout, "S Min",slider_s_min, input_s_min, lower_white[1], 255)
         
         input_v_min=QLineEdit()
         slider_v_min=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "V Min",slider_v_min, input_v_min, 0, 255)
+        slider_hsv(layout, "V Min",slider_v_min, input_v_min, lower_white[2], 255)
         
         input_h_max=QLineEdit()
         slider_h_max=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "H Max",slider_h_max, input_h_max, 179,179)
+        slider_hsv(layout, "H Max",slider_h_max, input_h_max, upper_white[0],179)
         
         input_s_max=QLineEdit()
         slider_s_max=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "S Max",slider_s_max, input_s_max, 255, 255)
+        slider_hsv(layout, "S Max",slider_s_max, input_s_max, upper_white[1], 255)
         
         input_v_max=QLineEdit()
         slider_v_max=QSlider(Qt.Horizontal)
-        slider_hsv(layout, "V Max",slider_v_max, input_v_max, 255, 255)
+        slider_hsv(layout, "V Max",slider_v_max, input_v_max, upper_white[2], 255)
+        
+        
+        
+        
+        lab_border_top=QLabel()
+        lab_border_top.setText("Border top")
+        layout.addWidget(lab_border_top)      
+        
+        input_border_top=QLineEdit()
+        input_border_top.setText(str(BORDER_UP))
+        layout.addWidget(input_border_top)
+        
+        lab_border_bottom=QLabel()
+        lab_border_bottom.setText("Border bottom")
+        layout.addWidget(lab_border_bottom)  
+        
+        input_border_bottom=QLineEdit()
+        input_border_bottom.setText(str(BORDER_DOWN))
+        layout.addWidget(input_border_bottom)
+        
+        lab_border_left=QLabel()
+        lab_border_left.setText("Border left")
+        layout.addWidget(lab_border_left)  
+        
+        input_border_left=QLineEdit()
+        input_border_left.setText(str(BORDER_LEFT))
+        layout.addWidget(input_border_left)
+        
+        lab_border_right=QLabel()
+        lab_border_right.setText("Border right")
+        layout.addWidget(lab_border_right)  
+        
+        input_border_right=QLineEdit()
+        input_border_right.setText(str(BORDER_RIGHT))
+        layout.addWidget(input_border_right)
         
         but_appy=QPushButton('Apply')
         layout.addWidget(but_appy)
@@ -500,9 +640,6 @@ def start():
         but_reset=QPushButton('Reset HSV  :')
         layout.addWidget(but_reset)
         but_reset.clicked.connect(reinit_hsv)
-        
-        
-        
         
         window.setLayout(layout)
         window.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint)
